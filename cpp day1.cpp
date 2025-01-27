9b8845a437df0c2de931e347cbbb051d97811606
SakshamBadole <sakshambadole19@gmail.com>
2025-01-27 13:51:12 +0530

    Add files via upload

---
+++ cpp day1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Hello World";
+}
+// sum
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cout<<"enter number:";
+	cin>>a>>b;
+	c=a+b;
+	cout<<"sum = "<<c;
+}
+
+//division
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cout<<"enter number:";
+	cin>>a>>b;
+	c=a/b;
+	cout<<"dev = "<<c;
+}
+
+// C & F pattern
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<" #######\n#\n#\n#####\n#\n#\n#\n ######\n ## ####\n#\n#\n#\n#\n#\n ## ##";
+	cin>>a;
+}
+
+// reverse of XML
+#include <iostream>
+using namespace std;
+int main(){
+	 char a,b,c;
+	cout<<"Enter character:";
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cout<<"reverse character:"<<c<<b<<a;
+}
+
+//paremeter area
+#include <iostream>
+using namespace std;
+int main(){
+	 int p,a, l=7, b=5;
+	 cout<<"parameter and area:\n";
+	 
+	 p=2*(l+b);
+	 a=l*b;
+	cout<<"perimenter="<<p;
+	cout<<"\narea= "<<a;
+}
